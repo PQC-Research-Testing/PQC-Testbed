d1c230afa95612c265a6d8675e4a4a3be3646692
Ryan <ryaneslovensky@gmail.com>
2025-04-16 12:29:35 -0400

    Removed unnecessary files

--- SDitH-v2/Optimized_Implementation/sdith_cat1_fast/generator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ryane/PQC-Testbed/SDitH-v2/Optimized_Implementation/sdith_cat1_fast/generator
-# Build directory: /home/ryane/PQC-Testbed/SDitH-v2/Optimized_Implementation/sdith_cat1_fast/generator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
